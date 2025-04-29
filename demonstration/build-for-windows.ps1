b52ac7fa60b132975b5cd9fb1dd4f9bcba0819ff
dotslashinit-sh <abinr5501@gmail.com>
2025-04-29 16:31:19 +0530

    Updated build-for-windows.ps1 / Make VSCode use powershell script

--- demonstration/build-for-windows.ps1
+++ demonstration/build-for-windows.ps1
@@ -1,8 +1,34 @@
-# Create a build directory
-if(-not(Test-Path ./build)) {
-    mkdir ./build
+function buildExe() {
+    # Configure CMake
+    cmake . -B build
+    # Build the file in release mode
+    $result = (cmake --build build --config Release )
+    Write-Output $result
+    if($result -eq 0) {
+        Write-Error "An error occurred when building the project file! Check logs for more details."
+    }
+    return $result
 }
 
-# Configure CMake
-cmake . -B build
-cmake --build build --config Release
+function copyExe() {
+    # Copies the exe file to the workspace root
+    Copy-Item ./build/Release/3d-game-shaders.exe ./
+}
+
+$firstTime = 1
+
+# Create a build directory
+if(-not(Test-Path ./build)) {
+    mkdir ./build > $null
+    $firstTime = 1
+}
+
+# If CMakeLists.txt or main.cxx is changed, rebuild the executable and copy it to the root directory
+if($firstTime -or -not(git diff --quiet CMakeLists.txt src/main.cxx)) {
+    Write-Output "Building exe file..."
+    if(buildExe -eq 0) {
+        Write-Output "Copying exe file..."
+        copyExe
+    }
+    Write-Output "Build completed successfully!"
+}
